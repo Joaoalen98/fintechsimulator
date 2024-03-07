6ac95543a2c34f83e83e9a018c0695c6916f3d8e
Joao Alencar <joao.alencar19982@gmail.com>
2024-03-06 22:38:58 -0300

    commit inicial

---
+++ src/main/java/com/joaoalencar/fintechsimulator/FintechsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.joaoalencar.fintechsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechsimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoalencar/fintechsimulator/FintechsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoalencar.fintechsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
